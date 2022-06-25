c7abbb0cfc709719a5b30aa5d09e808cca1eb506
lailaMoh <lailacloud2000@icloud.com>
2022-06-25 21:14:11 +0200

    database added

--- server/server.js
+++ server/server.js
@@ -1,3 +1,10 @@
+const mongoose = require("mongoose")
+const Document = require("./Document")
+
+mongoose.connect("mongodb://localhost/realTimeText", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 const io=require("socket.io")(3001,{
     cors:
      {
@@ -6,13 +13,21 @@ const io=require("socket.io")(3001,{
      },
 })
 io.on("connection", socket => {
-    socket.on('get-document', documentId =>{
-        const data=""
-        socket.join(documentId)
-        socket.emit("load-document", data)
+    socket.on('get-document',async documentId =>{
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId) //putting this socket in a room to handle documents separately
+        socket.emit("load-document",document.data)
+
     
         socket.on ('send-delta',delta =>{
             socket.broadcast.emit ("get-delta",delta) 
         })
     })  
 })
+async function findOrCreateDocument(id) {
+    if (id == null) return
+  
+    const document = await Document.findById(id)
+    if (document) return document
+    return await Document.create({ _id: id, data: defaultValue }) //default is empty string
+  }
