e36d0d18f2fb42949783c8fdf1c8590092a2d4bd
Nouran-saad <55962261+Nouran-saad@users.noreply.github.com>
2022-06-25 20:14:37 +0200

    make document id

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import TextEditior from './TextEdtior'
-
+import TextEdtior from "./TextEdtior";
+//import "./editorStyle.css"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import {v4 as uuidv4} from 'uuid'
 function App() {
+  const uid= uuidv4()
+  console.log(uid)
   return (
-    <TextEditior />
-  );
+  <Router>
+    <Routes>
+      <Route path="/"  element={<Navigate to={`/documents/${uuidv4()}`}/>}>
+  
+      </Route>
+      <Route path="/documents/:id" element={<TextEdtior/>}>
+
+      </Route>
+    </Routes> </Router> )
 }
 
 export default App;
